33473f1914445499f60bb6dc1b827894a43a301e
Big-oh-learn <shubhamingale779@gmail.com>
2024-11-13 21:51:34 +0530

    todo delete operation

---
+++ Del.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include "Task.c"
+#include "ipop.c"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Please provide a number as an argument.\n");
+        return 1;
+    }
+
+    char *endptr;
+    long int number = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0')
+    {
+        printf("Invalid input: %s is not a number.\n", argv[1]);
+        return 1;
+    }
+    readTodoListFromFile();
+    deleteTask(number);
+    writeTodoListToFile();
+    return 0;
+}
