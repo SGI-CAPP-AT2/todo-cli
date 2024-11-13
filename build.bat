af88b24ce73ccb41b21989c28a52ad6724fa018f
Big-oh-learn <shubhamingale779@gmail.com>
2024-11-13 21:51:58 +0530

    added build script

---
+++ build.bat
@@ -0,0 +1,5 @@
+mkdir bin
+gcc Show.c -o bin/task-show.exe
+gcc Add.c -o bin/task-add.exe
+gcc Del.c -o bin/task-del.exe
+gcc Complete.c -o bin/task-mark-complete.exe
---
+++ build.sh
@@ -0,0 +1,4 @@
+mkdir bin
+gcc Show.c -o bin/task-show.out
+gcc Add.c -o bin/task-add.out
+gcc Del.c -o bin/task-del.out
